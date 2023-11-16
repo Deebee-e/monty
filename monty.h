89ff16cc524b22cc321c5a5ffebc18cc18079217
Deebee-e <elideb4life@gmail.com>
2023-11-16 22:21:35 +0100

    update monty.h file

--- monty.h
+++ monty.h
@@ -42,6 +42,8 @@ void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number, char *arg_str);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void f_pop(stack_t **head, unsigned int counter);
+
 
 #define USAGE "USAGE: monty file\n"
 #define FILE_ERROR "Error: Can't open file %s\n"
